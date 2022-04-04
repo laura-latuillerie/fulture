229328787bdc124217648b5849b3ed8e241f6450
laura-latuillerie <laura.latuillerie@gmail.com>
2022-04-04 21:53:22 +0200

    Create Quote Block + Newsletter Block

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ $("#podcasts-filters li").click(function () {
   $(this).addClass("filter-active");
 
   var selectedFilter = $(this).data("filter");
-  $("#podcasts-wrapper").fadeTo(100, 0);
+  $(".podcasts-wrapper").fadeTo(100, 0);
 
   $(".podcasts-item").fadeOut().css("transform", "scale(0)");
 
   setTimeout(function () {
     $(selectedFilter).fadeIn(100).css("transform", "scale(1)");
-    $("#podcasts-wrapper").fadeTo(300, 1);
+    $(".podcasts-wrapper").fadeTo(300, 1);
   }, 300);
 });
